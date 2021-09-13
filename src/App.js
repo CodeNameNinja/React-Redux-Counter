1a9d1ec80e0f304ff0383687fbb4d664897a77d2
Mitchell <mwhyuen@gmail.com>
2021-09-13 11:35:02 +0200

    added basic authentication logic to form and nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Fragment } from 'react';
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
   return (
     <Fragment>
-    <Header />
-    <Auth />
-    <Counter />
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
     </Fragment>
   );
 }
